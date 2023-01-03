87128f05e899633b03feb6b4e81afd5f4a8c191c
Mehr ali <mehrali035@gmail.com>
2023-01-03 17:27:16 +0100

    two input handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
 
+import React, { useState } from 'react';
+import './App.js';
+
+
 function App() {
+  const [fullName, setFullName] = useState({
+
+    fname : '',
+    lname : '',
+  });
+
+
+  const inputEvent = (e) => {
+    // console.log(e.target.value);       // e is used to get the object and target.value get the specific value which user put in input field
+    // console.log(e.target.name);
+
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFullName((preValue) =>{
+      // console.log(preValue);    // preValue check tha previous value of state
+      if(name === 'fname'){
+        return{
+          fname : value,
+          lname : preValue.lname,
+        }
+      } else if(name === 'lname'){
+       return {
+        fname : preValue.fname,
+        lname : value,
+       }
+      }
+    })
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
-   <h1>Mehr Ali</h1>
-   <p> I am developer</p>
-   </>
+      <div className="main">
+        <form onSubmit={onSubmit}>
+          <h1>Hello {fullName.fname} {fullName.lname}</h1>
+
+          <input type="text" 
+          placeholder='Enter your Name' 
+          name='fname'
+          onChange={inputEvent} 
+          value={fullName.fname}/>
+
+          <br /><br />
+
+          <input type="Text" 
+          placeholder='Enter your password' 
+          name='lname'
+          onChange={inputEvent} 
+          value={fullName.lname} />
+
+
+          <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
 export default App;
-
